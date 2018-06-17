1f62ec608a6ef5ad0a9a9ac8b301f655bc90eeb0
Anshul <samgalacestyle01@gmail.com>
2018-06-17 13:02:53 +0530

    Modified the code for Pi Calculation to compare the built-in function with the mathematically calculated value.

--- PiToNthDigit_math_py_version.py
+++ PiToNthDigit_math_py_version.py
@@ -5,11 +5,23 @@ from math import pi
 
 
 def printPi(n):
+    # numerator for Pi
+    num = 22
+    # denominator for Pi
+    den = 7
+
+    # The code will compare the built in pi value with the calculated value(22/7).
+
     # innovative way to dynamically incorporate the precision specifier while using the format function.
     valPi = ("{:."+n+"f}").format(pi)
+    print(valPi)
+    valPi = ("{:." + n + "f}").format(float(num / den))
     print(valPi)
 
 
 if __name__ == '__main__':
-    n = input('Please specify the precision for Pi value to be printed:')
-    printPi(n)
+    f = 1
+    while(f == 1):
+        n = input('Please specify the precision for Pi value to be printed:')
+        printPi(n)
+        f = int(input('Run Again? (Yes-1/No-0):\n'))
