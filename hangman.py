16769cc6e297e1f4146def18ee52abb34b673a53
Anshul <samgalacestyle01@gmail.com>
2019-12-28 19:22:13 +0530

    Completed the code changes for hangman.py

--- hangman.py
+++ hangman.py
@@ -19,5 +19,61 @@ def get_num_attempts():
 def get_min_word_length():
     """ Get user-inputted minimum word length for the game."""
     while True:
-        min_word_length = input()
+        min_word_length = input('What minimum word length do you want? [4-16]: ')
+        try:
+            min_word_length = int(min_word_length)
+            if 4 <= min_word_length <= 16:
+                return min_word_length
+        
+        # Print the current game state
+        print('Word {0}'.format(get_display_word(word, idxs)))
+        print('Attempts Remaining: {0}'. format(attempts_remaining))
+        print('Previous Guesses: {0}'.format(' '.join(wrong_letters)))
+
+        # Get player's next letter guess
+        next_letter = get_next_letter(remaining_letters)
+
+        # Check if letter guess is in word
+        if next_letter in word:
+            # Guessed correctly
+            print('{0} is in the word!'.format(next_letter))
+
+            # Reveal matching letters
+            for i in range(len(word)):
+                if word[i] == next_letter:
+                    idxs[i] = True
+        else:
+            # Guessed incorrectly
+            print('{0} is NOT in the word!'.format(next_letter))
+
+            # Decrement num of attempts left and append guess to wrong guesses
+            attempts_remaining -= 1
+            wrong_letters.append(next_letter)
+        
+        # Check if the word is completely solved
+        if False not in idxs:
+            words_solved = True
+        print()
+    
+    # The game is over: reveal the word
+    print('The word is {0}'.format(word))
+
+    # Notify player of victory or defeat
+    if words_solved:
+        print('Congratulations! You won!')
+    else:
+        print('Try again next time!')
+
+    # Ask player if he/she wants to try again
+    try_again = input('Would you like to try again? [Y/N]: ')
+    return try_again.lower() == 'y'
+
+if __name__ == '__main__':
+    while play_hangman():
+        print()
+
+
+
+
+
 
